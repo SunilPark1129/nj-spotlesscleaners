9f1428586a907b3d81ac94dd06ff30c5feba1880
Sunil Park <sunilpark1129@gmail.com>
2024-05-26 09:22:34 -0400

    edited keywords

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,9 +9,9 @@ const poppins = Poppins({ weight: ["400", "700", "900"], subsets: ["latin"] });
 export const metadata = {
   title: { default: "Spotless Cleaners", template: "%s | Spotless Cleaners" },
   description:
-    "Welcome to Spotless Cleaners, your destination for top-quality garment care services in Sewell, NJ. Experience our exceptional cleaning and alteration services.",
+    "Welcome to Spotless Cleaners, your estination for top-quality garment care servicdes in Sewell, NJ. Experience our exceptional cleaning and alteration services.",
   keywords:
-    "Spotless Cleaners, garment care, Sewell NJ, cleaning services, alteration services",
+    "Spotless Cleaners, garment, Sewell, NJ, cleaning, dry cleaners, dry cleaning, alteration, repair, services",
   twitter: {
     card: "summary_large_image",
   },
