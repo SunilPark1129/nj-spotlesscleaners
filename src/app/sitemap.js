a021982dc4916b1819db8f45983950ff9720d6b2
Sunil Park <sunilpark1129@gmail.com>
2024-05-29 18:26:11 -0400

    added more properties

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,28 @@
 export default async function sitemap() {
   return [
-    { url: `https://spotlesscleaners-nj.com/` },
-    { url: `https://spotlesscleaners-nj.com/about` },
-    { url: `https://spotlesscleaners-nj.com/service` },
-    { url: `https://spotlesscleaners-nj.com/location` },
+    {
+      url: `https://spotlesscleaners-nj.com/`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+    {
+      url: `https://spotlesscleaners-nj.com/about`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: `https://spotlesscleaners-nj.com/service`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `https://spotlesscleaners-nj.com/location`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
   ];
 }
